7997c6f48d9a5da1843bf6a185c50ddd38402b92
Mike Bateman <mike.bateman@timeform.com>
2017-11-08 16:47:26 +0000

    Add empty constructor

--- src/BasketSizeReport.ts
+++ src/BasketSizeReport.ts
@@ -1,6 +1,5 @@
 export class BasketSizeReport {
-
-    public totalSizeMB: number;
-    public fileCount: number;
-
+  constructor() { };
+  public totalSizeMB: number;
+  public fileCount: number;
 }
